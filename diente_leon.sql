0e35b018bf9e4257253c83456dd882544d1d8a0d
sgmryu <sgmryusei@gmail.com>
2024-05-21 01:57:26 +0000

    archivos guardados

--- diente_leon.sql
+++ diente_leon.sql
@@ -94,13 +94,25 @@ ALTER TABLE socio
 ADD CONSTRAINT fk_sede_socio
 	FOREIGN KEY (idSocio) REFERENCES sede(idSocio);
     
+
+create table log_audit(
+id int primary key AUTO_INCREMENT
+usuario varchar(50),
+accion varchar(50),
+fecha_accion date,
+time_accion time
+);
 Create trigger trg_insert_cliente
 After insert on cliente
 For each row 
-Insert into tabla_cliente(idCliente,nombre_cliente,telefono_cliente,correo_cliente) values(NEW.idCliente,NEW.nombre_cliente,NEW.telefono_cliente,NEW.correo_cliente);
+Insert into log_audit(usuario,accion,fecha_accion, time_accion)
+values(current_user(),'insert', curdate(),now());
+
+insert into cliente values('joana', 1157894561,'joana@live.com');
 
 Create trigger trg_insert_empleado
 After insert on empleado
 For each row 
-Insert into tabla_empleado(idEmpleado,nombre_empleado,telefono_empleado) values(NEW.idEmpleado,NEW.nombre_empleado,NEW.telefono_empleado)
+Insert into tabla_empleado(idEmpleado,nombre_empleado,telefono_empleado) 
+values(NEW.idEmpleado,NEW.nombre_empleado,NEW.telefono_empleado)
 
