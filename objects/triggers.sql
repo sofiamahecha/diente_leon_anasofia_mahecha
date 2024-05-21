56cfb4183326a366e9badecfc85b062bafe47fb7
sgmryu <sgmryusei@gmail.com>
2024-05-21 02:27:00 +0000

    archivos guardados

--- objects/triggers.sql
+++ objects/triggers.sql
@@ -12,4 +12,16 @@ For each row
 Insert into log_audit(usuario,accion,fecha_accion, time_accion)
 values(current_user(),'insert', curdate(),now());
 
-insert into cliente(nombre_cliente,telefono_cliente,correo_cliente) values('joana', 1157894561,'joana@live.com');
+insert into cliente (nombre_cliente,telefono_cliente,correo_cliente)values('joana', 1157894561,'joana@live.com');
+-- select * from log_audit;
+
+-- despues de actualizar la tabla cliente, inserta el cambio realizado en la tabla de auditoría. 
+create trigger trg_updatecliente 
+after update on cliente 
+for each row 
+Insert into log_audit(usuario,accion,fecha_accion, time_accion)
+values(current_user(),'update', curdate(),now());
+set sql_safe_updates = 0 ;
+update cliente set correo_cliente = 'joana@live.com.ar' where correo_cliente like '%@live%';
+
+-- select * from cliente ;
